6ace8f9ed3c176ec2900e1fe5e8df78a7d7f265f
N Y <steelpipe75@gmail.com>
2026-01-18 00:06:42 +0900

    ウィンドウサイズ追従対応

--- pages/4_playground.py
+++ pages/4_playground.py
@@ -2,6 +2,7 @@ import streamlit as st
 import streamlit.components.v1 as components
 
 from utils import check_password
+from st_screen_stats import ScreenData
 
 # 認証チェック
 check_password()
@@ -10,7 +11,14 @@ check_password()
 def playground() -> None:
     st.title("playground")
 
-    components.iframe("https://steelpipe75.github.io/inhouse-ml-competition-playground-sample/")
+    screenD = ScreenData(setTimeout=1000)
+    data = screenD.st_screen_data()
+
+    components.iframe(
+        "https://steelpipe75.github.io/inhouse-ml-competition-playground-sample/",
+        width=data["innerWidth"],
+        height=int(data["innerHeight"]*0.9),
+    )
 
 
 playground()
